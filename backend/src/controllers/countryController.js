7cd0d27806d49603c72ef623b34bd64a88867410
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2021-09-22 22:31:38 -0300

    code countryController

--- backend/src/controllers/countryController.js
+++ backend/src/controllers/countryController.js
@@ -1,15 +1,70 @@
 //Responsible to get information from soap api and set in the google sheets db
+const { getSystemErrorMap } = require('util');
 const sheetConnection = require('../google-apis/spreadSheetConnection.js');
 
 module.exports = {
 
-    async getAllCountries() {
+    async getAllCountries(request, response, next) {
 
-        const sheet = sheetConnection.sheetsByIndex[0];
+        try{
 
-        const rows = await sheet.getRows();
+            const sheet = await sheetConnection.sheetsByIndex[0];
 
-        console.log(rows);
+            const rows = await sheet.getRows();
+
+            await sheet.loadCells('A1:B100');
+
+            response.end('Get countries success!');
+
+            next();
+        
+        }catch(error) {
+            console.log(error);
+            response.end(`Error: ${error}`);
+        };
+    },
+
+    async insertACountry(request, response) {
+
+        try{
+
+            const { sheetId } = request.params;
+
+            const sheet = await sheetConnection.sheetsByIndex[sheetId];
+
+            await sheet.addRow({
+                ISOCode: 'TESTE',
+                countryName: 'teste'
+            });
+
+            response.end('Success!');
+
+        }catch (error) {
+            console.log(error);
+            response.end(error);
+        }    
+    },
+
+    async insertListOfCountries(request, response) {
+
+        try{
+
+            const { sheetId } = request.params;
+
+            const sheet = await sheetConnection.sheetsByIndex[sheetId];
+
+            await sheet.addRows([
+                { ISOCode: 'TESTE', countryName: 'teste' },
+                { ISOCode: 'TESTE2', countryName: 'teste2'}            
+            ]);
+
+            response.end('Success!');
+
+        }catch (error) {
+            console.log(error);
+            response.end(error);
+        } 
+
     }
 };
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,10 +6,42 @@ const countryController = require('./controllers/countryController.js');
 
 const routes = express.Router();
 
-//test google sheets connection
-routes.get('/', countryController.getAllCountries)
+//get all countries from google sheets database
+routes.route('/')
+    .get(
+        countryController.getAllCountries,
+        countryController.insertListOfCountries
+    );
+
+//test to insert countrie in google sheets database
+routes.get('/insertACountry/:sheetId', countryController.insertACountry);
+
+//test to insert countrie in google sheets database
+routes.get('/insertListOfCountries/:sheetId', countryController.insertListOfCountries);
 
 //test soap api connection
 routes.get('test', soapApi.getListOfCountryNamesByCode);
+
+
+//express example for routes handler
+/*
+var cb0 = function (req, res, next) {
+    console.log('CB0')
+    next()
+  }
+  
+  var cb1 = function (req, res, next) {
+    console.log('CB1')
+    netx()
+  }
+  
+  routes.get('/example/d', [cb0, cb1], function (req, res, next) {
+    console.log('the response will be sent by the next function ...')
+    next()
+  }, function (req, res) {
+    res.send('Hello from D!')
+  })
+*/
+
 
 module.exports = routes;
